ad20a73e30c669a11fbabfc447b1650de506636b
Vlad <Vlad@Alex>
2017-11-15 20:09:10 +1300

    Mockito 2

--- Assignment3/src/test/java/Development/Assignment3/CalcImplementationTest.java
+++ Assignment3/src/test/java/Development/Assignment3/CalcImplementationTest.java
@@ -6,26 +6,52 @@ import junit.framework.TestCase;
 
 public class CalcImplementationTest extends TestCase {
 	
-	CalcImplementation obj;	
-	int a, b;
+	CalcImplementation obj;
+	int a,b;
 	protected void setUp() throws Exception {
-		obj = new CalcImplementation();
-		a = 3;
-		b = 5;
-		CalcInterface mockObj = mock(CalcInterface.class);
-		when(mockObj.add(a, b)).thenReturn(a+b);
-		obj.setCalc(mockObj);
-		
+	 obj=new CalcImplementation();
+	 a=3;
+	 b=5;
+	
+	 
+	 CalcInterface mockObj = mock(CalcInterface.class);
+	 when(mockObj.add(a, b)).thenReturn(a+b);
+	 obj.setCalc(mockObj);
+	 
+	 when(mockObj.sub(a, b)).thenReturn(a-b);
+	 obj.setCalc(mockObj);
+	 
+	 
+	 when(mockObj.mul(a, b)).thenReturn(a*b);
+	 obj.setCalc(mockObj);
+	 
+	 
+	 when(mockObj.division(a, b)).thenReturn((double) a/b);
+	 obj.setCalc(mockObj);
 	}
 
-	public void testAddTwoNums() {
+    public void testAddTwoNumbers() {
+    	assertEquals(8,obj.addTwoNumbers(3, 5));
 		
-		assertEquals(8, obj.addTwoNums(a, b));
 	}
-	
+    
+    public void testsubTwoNumbers() {
+    	assertEquals(8,obj.addTwoNumbers(3, 5));
+		
+	}
+    
+    public void testmulTwoNumbers() {
+    	assertEquals(8,obj.addTwoNumbers(3, 5));
+		
+	}
+    
+    public void testdivTwoNumbers() {
+    	assertEquals(8,obj.addTwoNumbers(3, 5));
+		
+	}
 	protected void tearDown() throws Exception {
-		obj = null;
-		a = 0;
-		b = 0;
+		obj=null;
+		a=0;
+		b=0;
 	}
 }
