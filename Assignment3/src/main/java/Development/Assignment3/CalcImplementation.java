d4cbf1d33897654e7b0413a3657ffc36e90216b5
Vlad <Vlad@Alex>
2017-11-16 19:39:34 +1300

    Tests modified

--- Assignment3/src/main/java/Development/Assignment3/CalcImplementation.java
+++ Assignment3/src/main/java/Development/Assignment3/CalcImplementation.java
@@ -7,8 +7,21 @@ public class CalcImplementation {
 	public void setCalc(CalcInterface calc) {
 		this.calc = calc;
 	}
-	
-	public int addTwoNums(int a, int b) {
+
+	public int addTwoNumbers(int a , int b) {
+		
 		return calc.add(a, b);
+	}
+    
+	public int  subTwonumbers(int a, int b) {
+		return calc.sub(a, b);
+	}
+	
+	public int mulTwonumbers(int a, int b) {
+		return calc.mul(a, b);
+	}
+	
+	public double divTwonumbers(int a, int b) {
+		return calc.division(a, b);
 	}
 }
--- Assignment3/src/main/java/Development/Assignment3/CalcInterface.java
+++ Assignment3/src/main/java/Development/Assignment3/CalcInterface.java
@@ -2,5 +2,11 @@ package Development.Assignment3;
 
 public interface CalcInterface {
 
-	public int add(int a, int b);
+	public int add(int a,int b);
+	   
+	   public int sub(int a,int b);
+	   
+	   public int mul(int a, int b);
+	   
+	   public double division(int a, int b);
 }
